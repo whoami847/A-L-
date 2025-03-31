05efc5504fc4f57e9100cafb29648f813af4518b
whoami847 <mrbeastbangla069@gmail.com>
2025-03-31 16:35:55 +0600

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "creepy-madeleine-whoami876-a83e05f5.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
